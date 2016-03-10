25d131dee352f86921290674e85e1b89497a0cb3
01-Liny <420471419@qq.com>
2016-03-10 23:15:59 +0800

    Boundry

---
+++ Assets/Scripts/DestoryByBoundary.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryByBoundary : MonoBehaviour {
+
+
+    void OnTriggerExit(Collider other)
+    {
+        // Destroy everything that leaves the trigger
+        Destroy(other.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
