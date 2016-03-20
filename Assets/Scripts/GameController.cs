0fd1e8b27cf95e921ceba47628a1b8e6082b2ba1
01-Liny <420471419@qq.com>
2016-03-20 20:36:47 +0800

    GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+    public GameObject hazard;
+    public Vector3 spawnValus;
+
+	// Use this for initialization
+	void Start () 
+    {
+        SpawnWaves();
+	}
+	
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValus.x, spawnValus.x), spawnValus.y, spawnValus.z);
+        Quaternion spawnRoation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRoation);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
