eddad92aeb9b6003f8d0e46d96b74b585eff679d
01-Liny <420471419@qq.com>
2016-03-20 16:23:12 +0800

    explosion

---
+++ Assets/Scripts/DestoryByContact.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryByContact : MonoBehaviour {
+
+    public GameObject explostion;
+    public GameObject playerExplostion;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explostion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplostion, other.transform.position, other.transform.rotation); 
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
